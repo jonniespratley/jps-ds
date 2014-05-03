6879a9bdd5d7c84f7c1dc3207f53dc3360118006
Jonnie Spratley <jonniespratley@me.com>
2014-05-03 14:47:12 -0700

    updated test and code

--- src/ds.js
+++ src/ds.js
@@ -4,14 +4,171 @@
  *
  * Copyright (c) 2014 Jonnie Spratley
  * Licensed under the MIT license.
+ *
+ * This is the resource object that contains all of the REST api methods for a full CRUD on a mongo account document.
  */
+/* global require, console */
+(function (exports) {
 
-(function(exports) {
+	'use strict';
+	exports.DS = function (options) {
 
-  'use strict';
+		var mongoose = require( "mongoose" );
+		var Deferred = require( "promised-io/promise" ).Deferred;
 
-  exports.awesome = function() {
-    return 'awesome';
-  };
+		/**
+		 * I am the instance of the ds connection.
+		 * @type {null}
+		 */
+		var instance = null;
 
-}(typeof exports === 'object' && exports || this));
+		/**
+		 * I hold a reference to all the models created.
+		 * @type {{}}
+		 */
+		var models = {};
+
+		/**
+		 * I am the reference to the ds object.
+		 * @type {{}}
+		 */
+		var ds = {};
+
+		/**
+		 * I handle connecting to the data source.
+		 * @param host
+		 * @returns {ds}
+		 */
+		ds.connect = function (host) {
+			if (options.models) {
+				for (var m in options.models) {
+					if (m) {
+						var model = mongoose.model( m, new mongoose.Schema( options.models[m] ) );
+						models[m] = model;
+					}
+				}
+			}
+			instance = mongoose.connect( "mongodb://" + host );
+			return this;
+		};
+
+		/**
+		 * I handle creating a new model.
+		 * @param name
+		 * @param schema
+		 * @returns {*}
+		 */
+		ds.model = function (name, schema) {
+			return mongoose.model( name, new mongoose.Schema( schema ) );
+		};
+
+		/**
+		 * I handle finding all documents.
+		 * @param table
+		 * @returns {Deferred.promise|*}
+		 */
+		ds.findAll = function (table) {
+			var deferred = new Deferred();
+			if (!models[table]) {
+				throw new Error( 'Must add table to options.' );
+			}
+
+			models[table].find( function (err, m) {
+				if (!err) {
+					deferred.resolve( m );
+				} else {
+					deferred.reject( err );
+				}
+			} );
+			return deferred.promise;
+		};
+
+		/**
+		 * I handle finding one document.
+		 * @param table
+		 * @param id
+		 * @returns {Deferred.promise|*}
+		 */
+		ds.findOne = function (table, id) {
+			var deferred = new Deferred();
+			if (!models[table]) {
+				throw new Error( 'Must add table to options.' );
+			}
+			models[table].findById( id, function (err, m) {
+				if (!err) {
+					deferred.resolve( m );
+				} else {
+					deferred.reject( err );
+				}
+			} );
+			return deferred.promise;
+		};
+
+		/**
+		 * I handle creating a new document.
+		 * @param table
+		 * @param data
+		 * @returns {Deferred.promise|*}
+		 */
+		ds.create = function (table, data) {
+			var deferred = new Deferred();
+			if (!models[table]) {
+				throw new Error( 'Must add table to options.' );
+			}
+			var model = new models[table]( data );
+			model.save( function (err, m) {
+				if (!err) {
+					deferred.resolve( m );
+
+				} else {
+					deferred.reject( err );
+				}
+			} );
+			return deferred.promise;
+		};
+
+		/**
+		 * I handle updating an existing document.
+		 * @param table
+		 * @param id
+		 * @param data
+		 * @returns {Deferred.promise|*}
+		 */
+		ds.update = function (table, id, data) {
+			var deferred = new Deferred();
+			models[table].findByIdAndUpdate( id, data, function (err, m) {
+				if (!err) {
+					deferred.resolve( m );
+				} else {
+					deferred.reject( err );
+				}
+			} );
+			return deferred.promise;
+		};
+
+		/**
+		 * I handle destroying a document.
+		 * @param table
+		 * @param id
+		 * @returns {Deferred.promise|*}
+		 */
+		ds.destroy = function (table, id) {
+			var deferred = new Deferred();
+			models[table].findByIdAndRemove( id, function (err, m) {
+				if (!err) {
+					deferred.resolve( m );
+				} else {
+					deferred.reject( err );
+				}
+			} );
+			return deferred.promise;
+		};
+
+		if (options.host) {
+			return ds.connect( options.host );
+		} else {
+			return ds;
+		}
+	};
+
+}( typeof exports === 'object' && exports || this ));
--- test/ds_test.js
+++ test/ds_test.js
@@ -1,36 +1,52 @@
 'use strict';
-
-var ds = require('../src/ds.js');
-
 /*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
+ ======== A Handy Little QUnit Reference ========
+ http://api.qunitjs.com/
 
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
+ Test methods:
+ module(name, {[setup][ ,teardown]})
+ test(name, callback)
+ expect(numberOfAssertions)
+ stop(increment)
+ start(decrement)
+ Test assertions:
+ ok(value, [message])
+ equal(actual, expected, [message])
+ notEqual(actual, expected, [message])
+ deepEqual(actual, expected, [message])
+ notDeepEqual(actual, expected, [message])
+ strictEqual(actual, expected, [message])
+ notStrictEqual(actual, expected, [message])
+ throws(block, [expected], [message])
+ */
+var DS = require( '../src/ds.js' ).DS;
+var _ds = new DS( {
+	host: 'localhost/learning-yeoman',
+	models: {
+		'pages': { title: String, slug: String, body: String, published: Boolean},
+		'users': { username: String, password: String, email: String, active: Boolean}
+	}
+} );
+//_db.connect( 'localhost/learning-yeoman' );
 
-exports['awesome'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(ds.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  }
+exports['DS'] = {
+	setUp: function (done) {
+
+		done();
+	},
+	'findAll': function (test) {
+		test.expect( 1 );
+		_ds.findAll( 'pages' ).then( function (data) {
+			test.ok( (data instanceof Array), 'should return array.' );
+			test.done();
+		} );
+	},
+	'findOne': function (test) {
+		test.expect( 1 );
+		var id = '5361b9244f0ca7e16196c997';
+		_ds.findOne( 'pages', id ).then( function (data) {
+			test.ok( (data instanceof Object), 'should return object.' );
+			test.done();
+		} );
+	}
 };
