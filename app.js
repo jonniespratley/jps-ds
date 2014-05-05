d89a2633f6d3f433ef85ec1f9942ccfb1e47a89a
Spratley, Jonathan Patrick <Jonnie.Spratley@ge.com>
2014-05-05 16:38:05 -0700

    updated to use local

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 var DS = require( './src/jps-ds.js' ).DS;
 var _ds = new DS( {
 	//host: 'localhost/learning-yeoman',
-	host: 'test:test@ds037498.mongolab.com:37498/learning-yeoman',
+	host: 'localhost/angular-cms',
 	models: {
 		'pages': { title: String, body: String, published: Boolean, created: Date}
 	}
 } );
 var pages = [];
-// _ds.create( 'pages', {
-	// title: 'Page ' + Date.now(),
-	// body: 'This is the page content.',
-	// published: true,
-	// created: new Date()
-// } ).then( function (page) {
-	// console.log( 'page created', page );
-// } );
+ _ds.create( 'pages', {
+	 title: 'Page ' + Date.now(),
+	 body: 'This is the page content.',
+	 published: true,
+	 created: new Date()
+ } ).then( function (page) {
+	 console.log( 'page created', page );
+ } );
 
 
 
